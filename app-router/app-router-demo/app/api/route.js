f1cb509a4159fdf616cb3f755037a57fff76b7a5
clarklindev <clarklindev@gmail.com>
2024-07-18 19:19:35 +0800

    fix nextjs deploy - remove api/route.js function

--- app-router/app-router-demo/app/api/route.js
+++ app-router/app-router-demo/app/api/route.js
@@ -1,7 +1,7 @@
-export function GET(request) {
-  console.log(request);
-  return new Response("hello");
-}
+// export function GET(request) {
+//   console.log(request);
+//   return new Response("hello");
+// }
 // export function POST(request){}
 // export function PATCH(request){}
 // export function PUT(request){}
