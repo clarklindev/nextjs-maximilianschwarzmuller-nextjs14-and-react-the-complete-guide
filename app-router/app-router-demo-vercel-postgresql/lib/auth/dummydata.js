cddce5f1a1743202278f6fdea76543c4354a9c58
clarklindev <clarklindev@gmail.com>
2024-07-22 11:51:50 +0800

    moved dummy data to external dummydata.js: import {DUMMY_DATA} from '@lib/auth/dummydata'

---
+++ app-router/app-router-demo-vercel-postgresql/lib/auth/dummydata.js
@@ -0,0 +1,38 @@
+export const DUMMY_DATA = [
+  {
+    title: "Yoga",
+    image: '/training/yoga.jpg',
+    description: 'A gentle way to improve flexibility and balance.'
+  },
+  {
+    title: 'Boxing',
+    image: '/training/boxing.jpg',
+    description: 'A high-energy workout that improves strength and speed.'
+  },
+  {
+    title: 'Running',
+    image: '/training/running.jpg',
+    description: 'A great way to improve cardiovascular health and endurance.'
+  },
+  {
+    title: 'Weightlifting',
+    image: '/training/weightlifting.jpg',
+    description: 'A strength-building workout that helps tone muscles.'
+  },
+  {
+    title: 'Cycling',
+    image: '/training/cycling.jpg',
+    description: 'A low-impact workout that improves cardiovascular health and endurance.'
+  },
+  {
+    title: 'Gaming',
+    image: '/training/gaming.jpg',
+    description: 'A fun way to improve hand-eye coordination and reflexes.'
+  },
+  {
+    title: 'Sailing',
+    image: '/training/sailing.jpg',
+    description: 'A relaxing way to enjoy the outdoors and improve balance.'
+  }
+];
+
--- app-router/app-router-demo-vercel-postgresql/lib/auth/initdb.js
+++ app-router/app-router-demo-vercel-postgresql/lib/auth/initdb.js
@@ -1,5 +1,6 @@
 import sql from "better-sqlite3";
 import path from "path";
+import { DUMMY_DATA } from "@/lib/auth/dummydata";
 
 // Assuming .db is in the /data directory relative to the root of your application
 const dbPath = path.join(process.cwd(), "data", "training.db");
@@ -29,27 +30,29 @@ function initdb() {
       description TEXT
     );
   `);
+}
+initdb();
 
-  const hasTrainings =
-    db.prepare("SELECT COUNT(*) as count FROM trainings").get().count > 0;
+function initData() {
+  const hasTrainings = db.prepare("SELECT COUNT(*) as count FROM trainings").get().count > 0;
 
   if (!hasTrainings) {
-    db.exec(`
-  INSERT INTO trainings (title, image, description)
-  VALUES
-  ('Yoga', '/training/yoga.jpg', 'A gentle way to improve flexibility and balance.'),
-  ('Boxing', '/training/boxing.jpg', 'A high-energy workout that improves strength and speed.'),
-  ('Running', '/training/running.jpg', 'A great way to improve cardiovascular health and endurance.'),
-  ('Weightlifting', '/training/weightlifting.jpg', 'A strength-building workout that helps tone muscles.'),
-  ('Cycling', '/training/cycling.jpg', 'A low-impact workout that improves cardiovascular health and endurance.'),
-  ('Gaming', '/training/gaming.jpg', 'A fun way to improve hand-eye coordination and reflexes.'),
-  ('Sailing', '/training/sailing.jpg', 'A relaxing way to enjoy the outdoors and improve balance.');
-`);
+    const stmt = db.prepare(`
+      INSERT INTO trainings VALUES (
+        null,
+        @title,
+        @image,
+        @description,
+      )`
+    );
+
+    for (const trainingmethod of DUMMY_DATA) {
+      stmt.run(trainingmethod);
+    }
   }
-
-  //DO NOT CLOSE DB CONNECTION
 }
+initData();
 
-initdb();
+//DO NOT CLOSE DB CONNECTION
 
 export default db;
