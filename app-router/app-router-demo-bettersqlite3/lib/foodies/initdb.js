fb62defa29c166abb4eaea75bd77260745e74a61
clarklindev <clarklindev@gmail.com>
2024-07-28 10:23:49 +0800

    rename app-router-demo-bettersqlite3 for bettersqlite3 example

---
+++ app-router/app-router-demo-bettersqlite3/lib/foodies/initdb.js
@@ -0,0 +1,44 @@
+import { DUMMY_DATA } from "@/lib/foodies/dummydata";
+
+const sql = require("better-sqlite3");
+import path from "path";
+
+// Assuming .db is in the /data directory relative to the root of your application
+const dbPath = path.join(process.cwd(), "data", "meals.db");
+const db = sql(dbPath);
+
+db.prepare(
+  `
+   CREATE TABLE IF NOT EXISTS meals (
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       slug TEXT NOT NULL UNIQUE,
+       title TEXT NOT NULL,
+       image TEXT NOT NULL,
+       summary TEXT NOT NULL,
+       instructions TEXT NOT NULL,
+       creator TEXT NOT NULL,
+       creator_email TEXT NOT NULL
+    )
+`
+).run();
+
+async function initData() {
+  const stmt = db.prepare(`
+      INSERT INTO meals VALUES (
+         null,
+         @slug,
+         @title,
+         @image,
+         @summary,
+         @instructions,
+         @creator,
+         @creator_email
+      )
+   `);
+
+  for (const meal of DUMMY_DATA) {
+    stmt.run(meal);
+  }
+}
+
+initData();
